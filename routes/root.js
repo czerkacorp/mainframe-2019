c6dedea28689a228ef368edbb3546076b160cad9
James McLean <jwmclean@users.noreply.github.com>
2018-12-04 21:54:23 -0500

    FIX fixed any instances of "citadel" appearing where "czerka" should

--- routes/root.js
+++ routes/root.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send({
-    title: "Citadel Mainframe",
+    title: "Czerka Mainframe",
     body: {
       message:
-        "Citadel Mainframe is the gateway to your SWGEmu Core3 MySQL server. It allows easy access to your database, so you can spend time building more client side applications.",
+        "Czerka Mainframe is the gateway to your SWGEmu Core3 MySQL server. It allows easy access to your database, so you can spend time building more client side applications.",
       instructions:
         "In order to use Mainframe, you should be calling the API endpoint, which is at https://your-host-here.com:port/api/v1"
     }
