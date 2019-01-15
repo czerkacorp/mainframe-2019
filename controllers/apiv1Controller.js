0cbc91dbc7c9f69dcdc58760ac35afa164ebb0eb
James McLean <jmcleanbos@gmail.com>
2019-01-15 01:03:25 -0500

    ADD - quotes.json and a random quote function to helpers.js to test fuctionality in apiv1Controller.js, UPDATE - endpoints that return data to follow best practice by giving a status code then the data, UPDATE - default config and eexample config with default swgemu DB secret

--- controllers/apiv1Controller.js
+++ controllers/apiv1Controller.js
@@ -2,10 +2,11 @@
 const h = require('../lib/helpers');
 
 exports.home = (req, res) => {
-  res.send({
+  res.status(200).send({
     title: 'API Endpoint, woohoo',
     body: {
       message: 'I have a bad feeling about this......',
+      random_quote: h.randomQuote(),
     },
   });
 };
